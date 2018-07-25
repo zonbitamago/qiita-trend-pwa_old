6a02d1a81d9894ab34571129ed216eeb6c91f1f6
kazuma takiuchi <basstrombone413@gmail.com>
2018-07-26 05:07:39 +0900

    fix dynamic pages

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
 import Layout from "../components/MyLayout";
+import Link from "next/link";
+
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
 
 const Index = () => (
   <Layout>
-    <main>Welcome to next.js!</main>
+    <main>
+      <h1>Welcome to next.js!</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awsome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
+    </main>
   </Layout>
 );
 
---
+++ pages/post.js
@@ -0,0 +1,11 @@
+import { withRouter } from "next/router";
+import Layout from "../components/MyLayout.js";
+
+const Page = withRouter(props => (
+  <Layout>
+    <h1>{props.router.query.title}</h1>
+    <p>This is the blog post content.</p>
+  </Layout>
+));
+
+export default Page;
