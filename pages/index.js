4be32a4fbb42fc48f0c6bffa56f8f5a500b2cff3
kazuma takiuchi <basstrombone413@gmail.com>
2018-07-29 07:38:07 +0900

    add styling component

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,65 @@
-import Layout from "../components/MyLayout";
+import Layout from "../components/MyLayout.js";
 import Link from "next/link";
-import fetch from "isomorphic-unfetch";
 
-// const PostLink = props => (
-//   <li>
-//     <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-//       <a>{props.title}</a>
-//     </Link>
-//   </li>
-// );
+function getPosts() {
+  return [
+    { id: "hello-nextjs", title: "Hello Next.js" },
+    { id: "learn-nextjs", title: "Learn Next.js is awesome" },
+    { id: "deploy-nextjs", title: "Deploy apps with ZEIT" }
+  ];
+}
 
-const Index = props => (
-  <Layout>
-    <main>
-      <h1>Batman TV Shows</h1>
-      <ul>
-        {props.shows.map(({ show }) => (
-          <li key={show.id}>
-            <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-              <a>{show.name}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </main>
-  </Layout>
+const PostLink = ({ post }) => (
+  <li>
+    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+      <a>{post.title}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
 );
 
-Index.getInitialProps = async function() {
-  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
-  const data = await res.json();
+export default () => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>{getPosts().map(post => <PostLink key={post.id} post={post} />)}</ul>
+    <style jsx>{`
+      h1,
+      a {
+        font-family: "Arial";
+      }
 
-  console.log(`Show data fetched. Count: ${data.length}`);
+      ul {
+        padding: 0;
+      }
 
-  return {
-    shows: data
-  };
-};
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
 
-export default Index;
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </Layout>
+);
