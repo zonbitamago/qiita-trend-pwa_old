53171cb8c797681418a175ba0ef03b46aebb77d4
kazuma takiuchi <basstrombone413@gmail.com>
2018-07-28 09:59:58 +0900

    fix isomorfic-fetch

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,41 @@
 import Layout from "../components/MyLayout";
 import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
-const PostLink = props => (
-  <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
-);
+// const PostLink = props => (
+//   <li>
+//     <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+//       <a>{props.title}</a>
+//     </Link>
+//   </li>
+// );
 
-const Index = () => (
+const Index = props => (
   <Layout>
     <main>
-      <h1>Welcome to next.js!</h1>
+      <h1>Batman TV Shows</h1>
       <ul>
-        <PostLink id="hello-nextjs" title="Hello Next.js" />
-        <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
-        <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+        {props.shows.map(({ show }) => (
+          <li key={show.id}>
+            <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+              <a>{show.name}</a>
+            </Link>
+          </li>
+        ))}
       </ul>
     </main>
   </Layout>
 );
+
+Index.getInitialProps = async function() {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data
+  };
+};
 
 export default Index;
--- pages/post.js
+++ pages/post.js
@@ -1,11 +1,22 @@
-import { withRouter } from "next/router";
 import Layout from "../components/MyLayout.js";
+import fetch from "isomorphic-unfetch";
 
-const Page = withRouter(props => (
+const Post = props => (
   <Layout>
-    <h1>{props.router.query.title}</h1>
-    <p>This is the blog post content.</p>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, "")}</p>
+    <img src={props.show.image.medium} />
   </Layout>
-));
+);
 
-export default Page;
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
+};
+
+export default Post;
