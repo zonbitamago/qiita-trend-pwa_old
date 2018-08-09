d809c1fad7605f2732fd2dbac0681bc779a3bd6c
kazuma takiuchi <basstrombone413@gmail.com>
2018-08-10 05:11:35 +0900

    change fech timing

--- components/Meta.js
+++ components/Meta.js
@@ -6,6 +6,8 @@ const Meta = () => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
+      <meta name="Description" content="Qiita trend information." />
+      <title>Qiita-Trend-PWA</title>
     </Head>
     <ServiceWorkerRegister />
     <style jsx global>{`
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Layout from "../components/MyLayout.js";
 import fetch from "isomorphic-unfetch";
 import Item from "../components/Item";
@@ -9,49 +10,99 @@ const itemContainerStyle = {
   gridGap: " 0.8em"
 };
 
-const Index = props => {
-  let daily = props.daily.map((elem, idx) => {
-    let name = elem.slice(elem.indexOf("[") + 1, elem.indexOf("]"));
-    let url = elem.slice(elem.indexOf("(") + 1, elem.indexOf(")"));
-    let rate = elem.slice(elem.lastIndexOf("("));
-    return <Item key={idx} idx={idx} url={url} name={name} rate={rate} />;
-  });
+// const Index = props => {
+//   let daily = props.daily.map((elem, idx) => {
+//     let name = elem.slice(elem.indexOf("[") + 1, elem.indexOf("]"));
+//     let url = elem.slice(elem.indexOf("(") + 1, elem.indexOf(")"));
+//     let rate = elem.slice(elem.lastIndexOf("("));
+//     return <Item key={idx} idx={idx} url={url} name={name} rate={rate} />;
+//   });
 
-  let weekly = props.weekly.map((elem, idx) => {
-    let name = elem.slice(elem.indexOf("[") + 1, elem.indexOf("]"));
-    let url = elem.slice(elem.indexOf("(") + 1, elem.indexOf(")"));
-    let rate = elem.slice(elem.lastIndexOf("("));
-    return <Item key={idx} idx={idx} url={url} name={name} rate={rate} />;
-  });
+//   let weekly = props.weekly.map((elem, idx) => {
+//     let name = elem.slice(elem.indexOf("[") + 1, elem.indexOf("]"));
+//     let url = elem.slice(elem.indexOf("(") + 1, elem.indexOf(")"));
+//     let rate = elem.slice(elem.lastIndexOf("("));
+//     return <Item key={idx} idx={idx} url={url} name={name} rate={rate} />;
+//   });
 
-  return (
-    <Layout>
-      <h1>Trend</h1>
-      <h2>Dailyランキング</h2>
-      <div style={itemContainerStyle}>{daily}</div>
-      <h2>Weeklyランキング</h2>
-      <div style={itemContainerStyle}>{weekly}</div>
-    </Layout>
-  );
-};
+//   return (
+//     <Layout>
+//       <h1>Trend</h1>
+//       <h2>Dailyランキング</h2>
+//       <div style={itemContainerStyle}>{daily}</div>
+//       <h2>Weeklyランキング</h2>
+//       <div style={itemContainerStyle}>{weekly}</div>
+//     </Layout>
+//   );
+// };
 
-Index.getInitialProps = async function() {
-  const res = await fetch(
-    "https://qiita.com/api/v2/items/bb154a4bc198fb102ff3"
-  );
-  const data = await res.json();
+// Index.getInitialProps = async function() {
+//   const res = await fetch(
+//     "https://qiita.com/api/v2/items/bb154a4bc198fb102ff3"
+//   );
+//   const data = await res.json();
 
-  let daily = data.body
-    .split("# ウィークリーいいねランキング")[0]
-    .split("####")
-    .slice(1);
+//   let daily = data.body
+//     .split("# ウィークリーいいねランキング")[0]
+//     .split("####")
+//     .slice(1);
 
-  let weekly = data.body
-    .split("# ウィークリーいいねランキング")[1]
-    .split("####")
-    .slice(1);
+//   let weekly = data.body
+//     .split("# ウィークリーいいねランキング")[1]
+//     .split("####")
+//     .slice(1);
 
-  return { daily: daily, weekly: weekly };
-};
+//   return { daily: daily, weekly: weekly };
+// };
+
+class Index extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      daily: [],
+      weekly: []
+    };
+  }
+  componentDidMount = async function() {
+    const res = await fetch(
+      "https://qiita.com/api/v2/items/bb154a4bc198fb102ff3"
+    );
+    const data = await res.json();
+    let daily = data.body
+      .split("# ウィークリーいいねランキング")[0]
+      .split("####")
+      .slice(1);
+    let weekly = data.body
+      .split("# ウィークリーいいねランキング")[1]
+      .split("####")
+      .slice(1);
+    this.setState({ daily: daily, weekly: weekly });
+  };
+  render() {
+    let daily = this.state.daily.map((elem, idx) => {
+      let name = elem.slice(elem.indexOf("[") + 1, elem.indexOf("]"));
+      let url = elem.slice(elem.indexOf("(") + 1, elem.indexOf(")"));
+      let rate = elem.slice(elem.lastIndexOf("("));
+      return <Item key={idx} idx={idx} url={url} name={name} rate={rate} />;
+    });
+
+    let weekly = this.state.weekly.map((elem, idx) => {
+      let name = elem.slice(elem.indexOf("[") + 1, elem.indexOf("]"));
+      let url = elem.slice(elem.indexOf("(") + 1, elem.indexOf(")"));
+      let rate = elem.slice(elem.lastIndexOf("("));
+      return <Item key={idx} idx={idx} url={url} name={name} rate={rate} />;
+    });
+
+    return (
+      <Layout>
+        <h1>Trend</h1>
+        <h2>Dailyランキング</h2>
+        <div style={itemContainerStyle}>{daily}</div>
+        <h2>Weeklyランキング</h2>
+        <div style={itemContainerStyle}>{weekly}</div>
+      </Layout>
+    );
+  }
+}
 
 export default Index;
