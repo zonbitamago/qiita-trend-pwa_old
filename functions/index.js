f0419ae9df02483ed91f125a2561f042c44acaf1
kazuma takiuchi <basstrombone413@gmail.com>
2018-08-17 05:16:51 +0900

    fix cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 const functions = require("firebase-functions");
+const unfetch = require("isomorphic-unfetch");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
+  let fetch = async function() {
+    return 1;
+  };
+
+  fetch();
   response.send("Hello from Firebase!");
 });
