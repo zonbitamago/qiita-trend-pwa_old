50070182046f8a982d93c3c58f6f8ac7cbf5b5af
kazuma takiuchi <basstrombone413@gmail.com>
2018-08-18 08:53:07 +0900

    fix functions

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,19 @@
 const functions = require("firebase-functions");
-const unfetch = require("isomorphic-unfetch");
+const fetch = require("isomorphic-unfetch");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  let fetch = async function() {
-    return 1;
-  };
-
-  fetch();
-  response.send("Hello from Firebase!");
+  fetch("https://qiita.com/api/v2/items/bb154a4bc198fb102ff3")
+    .then(res => {
+      return res.json();
+    })
+    .then(json => {
+      response.send("res:" + JSON.stringify(json));
+      return;
+    })
+    .catch(error => {
+      response.send("error:" + error);
+    });
 });
