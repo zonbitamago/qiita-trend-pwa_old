175523e2aab40c107c8a8cf4dc68763f5bddffa7
kazuma takiuchi <basstrombone413@gmail.com>
2018-10-08 00:13:40 +0900

    fix title tag

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,6 @@ export default class MyDocument extends Document {
     return (
       <html lang="ja">
         <Head>
-          <title>Qiita-Trend-PWA</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
           <meta
