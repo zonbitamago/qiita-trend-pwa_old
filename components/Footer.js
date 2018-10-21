28a480913a8e06cc455d11587f2bae7280f4c44e
kazuma takiuchi <basstrombone413@gmail.com>
2018-10-22 08:59:17 +0900

    add footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,14 @@
-const Footer = () => <footer>footer</footer>;
+const footerStyle = {
+  margin: "0px 10px 8px 0px",
+  display: "flex",
+  justifyContent: "flex-end",
+  fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  fontSize: "1rem",
+  fontWeight: "300"
+};
+
+const Footer = () => (
+  <footer style={footerStyle}>©2018, made with zonbitamago</footer>
+);
 
 export default Footer;
--- components/MyLayout.js
+++ components/MyLayout.js
@@ -1,4 +1,5 @@
 import Header from "./Header";
+import Footer from "./Footer";
 
 const mainStyle = {
   marginTop: "4em"
@@ -7,6 +8,7 @@ const Layout = props => (
   <div>
     <Header />
     <main style={mainStyle}>{props.children}</main>
+    <Footer />
   </div>
 );
 
